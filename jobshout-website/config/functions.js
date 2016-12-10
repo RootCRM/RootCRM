38712b70b94af8e701b92ab87d564779cd609282
Balinder Walia <bwalia@tenthmatrix.co.uk>
2016-12-10 11:23:59 +0000

    jobshout_website_added in RootCRM code base

---
+++ jobshout-website/config/functions.js
@@ -0,0 +1,54 @@
+	/**********************************************************************
+	*  Author: Neha Kapoor (neha@rootcrm.org)
+	*  Project Lead: Balinder WALIA (bwalia@rootcrm.org)
+	*  Project Lead Web...: https://twitter.com/balinderwalia
+	*  Name..: ROOTCRM
+	*  Desc..: Root CRM (part of RootCrm Suite of Apps)
+	*  Web: http://rootcrm.org
+	*  License: http://rootcrm.org/LICENSE.txt
+	**/
+
+	/**********************************************************************
+	*  functions.js contain all the functions required for requests
+	**/
+	
+	var init = require('./init');
+	var mongodb=init.mongodb;
+
+module.exports = 
+{
+  	// These functions which will be called in the main file, which is server.js
+  	returnFindOneByMongoID : function (db, collectionName, search_id, cb){
+		var outputObj = new Object();
+		db.collection(collectionName).findOne({_id: new mongodb.ObjectID(search_id)}, function(err, document_details) {
+			if (err) {
+				outputObj["error"]   = err;
+				cb(outputObj);
+      		} else if (document_details) {
+      			outputObj["aaData"]   = document_details;
+				cb(outputObj);
+     		}
+		});
+	},
+	
+	nowTimestamp : function (){
+		var timeStampStr=Math.round(new Date().getTime()/1000)
+		return timeStampStr;
+	},
+	
+	returnNavigation : function (db, cb){
+		db.collection('bookmarks').find({"Status": { $in: [ 1, "1" ] } , categories: { $in: [ 'footer-nav', 'top-navigation' ] }}).sort( { order_by_num: 1 } ).toArray(function(err, tokens_result) {
+			if(err) return cb(null)
+			cb(tokens_result);
+		});
+	},
+	
+	guid : function () {
+  		function s4() {
+    		return Math.floor((1 + Math.random()) * 0x10000)
+      		.toString(16)
+      		.substring(1);
+  		}
+  		return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+	}
+};
---
+++ jobshout-website/config/init.js
@@ -0,0 +1,26 @@
+	/**********************************************************************
+	*  Author: Neha Kapoor (neha@rootcrm.org)
+	*  Project Lead: Balinder WALIA (bwalia@rootcrm.org)
+	*  Project Lead Web...: https://twitter.com/balinderwalia
+	*  Name..: ROOTCRM
+	*  Desc..: Root CRM (part of RootCrm Suite of Apps)
+	*  Web: http://rootcrm.org
+	*  License: http://rootcrm.org/LICENSE.txt
+	**/
+
+	/**********************************************************************
+	*  init.js
+	**/
+	
+	var mongodbRe = require('mongodb')
+	var MongoClient = mongodbRe.MongoClient;
+	
+	// Connection URL. This is where your mongodb server is running.
+	var url = 'mongodb://localhost:27017/jobshout_live';
+	var _db;
+	module.exports = {
+    	mongodb : mongodbRe,
+    	MongoClient : MongoClient,
+    	mongoConnUrl : url,
+    	port : 3000
+	};
