6dc829f4ccf7aab011b947a4ec6df4ab8cd329ff
neha-kapoor <neha.kapoor@tenthmatrix.co.uk>
2016-12-13 12:45:12 +0530

    Changed database name for jobshout website

--- jobshout-website/config/init.js
+++ jobshout-website/config/init.js
@@ -16,7 +16,7 @@
 	var MongoClient = mongodbRe.MongoClient;
 	
 	// Connection URL. This is where your mongodb server is running.
-	var url = 'mongodb://localhost:27017/jobshout_db';
+	var url = 'mongodb://localhost:27017/jobshout_live';
 	var _db;
 	module.exports = {
     	mongodb : mongodbRe,
